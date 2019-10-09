9e077650572f0897094e919bf840e80ebf944fe3
geatrigger <geatrigger@gmail.com>
2019-10-09 21:40:01 +0900

    Add sub mul div in calc.js

--- calc.js
+++ calc.js
@@ -4,6 +4,12 @@ let ex = module.exports = {};
 ex.calc = function(a, b, op) {
   if(op == "add")
     return tmp.add(a, b);
+  else if(op == "sub")
+    return tmp.sub(a, b);
+  else if(op == "mul")
+    return tmp.mul(a, b);
+  else if(op == "div")
+    return tmp.div(a, b);
   else if(op == "memory")
     return tmp.mem;
 };
@@ -18,3 +24,15 @@ Calc.prototype.add = function(a, b) {
     return this.mem;
 };
 
+Calc.prototype.sub = function(a, b) {
+    this.mem = a - b;
+    return this.mem;
+};
+Calc.prototype.mul = function(a, b) {
+    this.mem = a * b;
+    return this.mem;
+};
+Calc.prototype.div = function(a, b) {
+    this.mem = a / b;
+    return this.mem;
+};
